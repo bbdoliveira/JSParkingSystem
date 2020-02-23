efaf9a3b9a94fa3d489f70f6ecf32f9e8ac2433c
Bruno Bonifácio de Oliveira <bbdoliveira@gmail.com>
2020-02-23 15:37:45 -0300

    Parou de Funcionar

--- js/main.js
+++ js/main.js
@@ -23,6 +23,41 @@ function cadastraVeiculo(e) {
     }
     //localStorage.setItem('Teste', 'Olá Mundo!')
     //console.log(carro);
-
+    mostraPatio()
     e.preventDefault();
+}
+
+function apagarVeiculo(placa) {
+    var carros = JSON.parse(localStorage.getItem('patio2')) //Recupera o que tem no patio.
+
+    for (let i = 0; i < carros.length; i++) {
+        if(carros[i].placa == placa){
+            carros.splice(i, 1)
+        }
+
+        localStorage.setItem('patio2', JSON.stringify(carros))
+    }
+
+    mostraPatio()
+
+}
+
+function mostraPatio() {
+    let carros = JSON.parse(localStorage.getItem('patio2'))
+    let carrosResultado = document.getElementById('resultados')
+
+    carrosResultado.innerHTML = ''
+
+    for (let i = 0; i < carros.length; i++) {
+        let modelo = carros[i].modelo
+        let placa = carros[i].placa
+        let hora = carros[i].hora
+        let minutos = carros[i].minutos
+
+        carrosResultado.innerHTML += '<tr><td>' + modelo + 
+                               '</td><td>' + placa +
+                               '</td><td>' + hora + ' : ' + minutos +
+                               '</td><td><button class="btn btn-danger" onclick="apagarVeiculo(\'' + placa + '\')">Excluir</button></td>' +
+                               '</tr>'
+    }
 }
